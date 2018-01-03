99fd013d706bb64bea4e49b21d515c511dbbc712
rYin <18192392646@163.com>
2018-01-03 22:19:14 +0800

    demo

---
+++ pillow-demo/demo01.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+
+from PIL import Image
+
+im = Image.open("C:/Users/YR/Pictures/girfriend/1.jpg")
+print(im.format, im.size, im.mode)
---
+++ readme.md
@@ -0,0 +1,2 @@
+# readme
+个人python学习仓库，使用Python版本3.6
